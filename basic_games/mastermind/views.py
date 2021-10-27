792bbfb10aaa703d11ac88c09f4b786a396dbc82
smzizi021 <smzizi021@student.wethinkcode.co.za>
2021-10-27 09:51:51 +0200

    Game Base Code

---
+++ basic_games/mastermind/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+import random
+
+   
+def code_gen():
+    code = []
+    while len(code) != 4:
+        digit = random.randint(0,9)
+        if digit not in code:
+            code.append(str(digit))
+
+    return code
+
+
+
+# Create your views here.
+def game_home(request):
+    
+    return render(request,"mastermind/home.html",{
+        "code":code_gen(),
+
+    })
